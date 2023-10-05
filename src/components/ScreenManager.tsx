20bfc8adb17c9f1117a680b4fc3426a695507acd
rvyk <szymanskipawel327@gmail.com>
2023-10-05 19:05:54 +0200

    Next.js Init

--- src/components/ScreenManager.tsx
+++
@@ -1,8 +0,0 @@
-interface Props {
-  screens: JSX.Element[];
-  activeByIndex: number;
-}
-
-export default function ScreenManager({ screens, activeByIndex }: Props) {
-  return screens[activeByIndex];
-}
